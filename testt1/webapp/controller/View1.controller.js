ac382d822a79a2d694d0ae1bfcd035df15ea2f0b
jyoti patil <jtpatil43@gmail.com>
2019-07-02 09:58:48 +0000

    Test app

---
+++ testt1/webapp/controller/View1.controller.js
@@ -0,0 +1,11 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller"
+], function (Controller) {
+	"use strict";
+
+	return Controller.extend("testn.com.testt1.controller.View1", {
+		onInit: function () {
+
+		}
+	});
+});
